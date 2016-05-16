f06b1206e7ba84bb012bed0be31b518bb784ed14
timnew <timnew@users.noreply.github.com>
2016-05-16 14:25:50 +0800

    Support tag version in gulp

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,32 +1,24 @@
 import gulp from 'gulp'
 import bump from 'gulp-bump'
+import git from 'gulp-git'
+import tagVersion from 'gulp-tag-version'
 import start from 'gulp-start-process'
 import moment from 'moment'
 global.moment = moment
 
 gulp.task('default', ['spec'])
 
-gulp.task('server', ['server:dev'])
-gulp.task('server:dev', (done) => {
-  start('nodemon bin/server | bunyan', done)
-})
-gulp.task('server:prod', (done) => {
-  start('pm2 bin/server', done)
-})
-
 ;['major', 'minor', 'patch'].forEach((type) => {
   gulp.task(`bump:${type}`, ['build'], () =>
     gulp.src('./package.json')
     .pipe(bump({ type }))
     .pipe(gulp.dest('./'))
+    .pipe(git.commit('bumps version'))
+    .pipe(tagVersion())
   )
 })
 gulp.task('bump', ['bump:patch'])
 
 gulp.task('spec', (done) => {
   start('NODE_ENV=test ./node_modules/.bin/mocha --harmony --opts mocha.opts "specs/**/*Spec.js"', done)
-})
-
-gulp.task('remote:log', (done) => {
-  start('ssh server "tail -f ~/node/tickets/shared/logs/tickets.log" | bunyan', done)
 })
